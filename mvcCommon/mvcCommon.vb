9c56095346647110f1b97d4281eb5f3936d5bb28
Joshua Foster <josh@contensive.com>
2013-12-19 13:04:44 -0500

    Initial push
    added project and set up sample

---
+++ mvcCommon/mvcCommon.vb
@@ -0,0 +1,78 @@
+﻿Namespace Contensive.Addons.mvcProject
+    '
+    Public Module mvcCommon
+        '
+        Public errList As New List(Of jsonPackageError)
+        '
+        Public Function getSerializedJSONWrapper(ByVal CP As Contensive.BaseClasses.CPBaseClass, ByVal success As Boolean, Optional ByVal errorObj As List(Of jsonPackageError) = Nothing, Optional ByVal dataObj As List(Of jsonPackageData) = Nothing, Optional ByVal designObject As List(Of jsonPackageData) = Nothing) As String
+            Dim s As String = ""
+            '
+            Try
+                Dim package As New jsonPackage
+                '
+                package.success = success
+                package.dataError = errorObj
+                package.data = dataObj
+                package.design = designObject
+                '
+                s = serializeObject(CP, package)
+                '
+                errorObj.RemoveRange(0, errorObj.Count)
+                dataObj.RemoveRange(0, errorObj.Count)
+            Catch ex As Exception
+                Try
+                    CP.Site.ErrorReport(ex, "error in Contensive.Addons.mvcProject.mvcCommon.getSerializedJSONWrapper")
+                Catch errObj As Exception
+                End Try
+            End Try
+            '
+            Return s
+        End Function
+        '
+        Public Function serializeObject(ByVal CP As Contensive.BaseClasses.CPBaseClass, ByVal dataObject As Object) As String
+            Dim s As String = ""
+            '
+            Try
+                s = Newtonsoft.Json.JsonConvert.SerializeObject(dataObject)
+            Catch ex As Exception
+                Try
+                    CP.Site.ErrorReport(ex, "error in Contensive.Addons.mvcProject.mvcCommon.serializeObject")
+                Catch errObj As Exception
+                End Try
+            End Try
+            '
+            Return s
+        End Function
+        '
+    End Module
+    '
+    <Serializable()>
+    Public Class jsonPackage
+        Public success As Boolean = False
+        Public dataError As New List(Of jsonPackageError)
+        Public data As New List(Of jsonPackageData)
+        Public designError As New List(Of jsonPackageError)
+        Public design As New List(Of jsonPackageData)
+    End Class
+    '
+    <Serializable()>
+    Public Class jsonPackageData
+        Public dataFor As String = ""
+        Public recordCount As Integer = 0
+        Public dataSet As Object
+    End Class
+    '
+    <Serializable()>
+    Public Class jsonPackageError
+        Public number As Integer = 0
+        Public description As String = ""
+    End Class
+    '
+    <Serializable()>
+    Public Class people
+        Public id As Integer = 0
+        Public firstName As String = ""
+        Public lastName As String = ""
+    End Class
+    '
+End Namespace
---
+++ mvcProject1/mvcProject1Class.vb
@@ -0,0 +1,96 @@
+﻿Namespace Contensive.Addons.mvcProject
+    '
+    Public Class mvcProject1Class
+        Inherits BaseClasses.AddonBaseClass
+        '
+        Public Overrides Function Execute(ByVal CP As BaseClasses.CPBaseClass) As Object
+            Dim s As String = ""
+            '
+            Try
+                s = CP.Html.div(s, , , "mvcContainer")
+            Catch ex As Exception
+                Try
+                    CP.Site.ErrorReport(ex, "error in Contensive.Addons.performanceCloud.effectRemotes.mvcProject1Class.Execute")
+                Catch errObj As Exception
+                End Try
+            End Try
+            '
+            Return s
+        End Function
+        '
+    End Class
+    '
+    Public Class mvcProject1HandlerClass
+        Inherits BaseClasses.AddonBaseClass
+        '
+        Public Overrides Function Execute(ByVal CP As BaseClasses.CPBaseClass) As Object
+            Dim s As String = ""
+            '
+            Try
+                Dim cs As BaseClasses.CPCSBaseClass = CP.CSNew()
+                Dim people As New people
+                Dim peopleList As New List(Of people)
+                '
+                Dim layout As String = ""
+                '
+                Dim localPackageDesign As New jsonPackageData
+                Dim localPackageData As New jsonPackageData
+                Dim localPackageError As New jsonPackageError
+                Dim success As Boolean = False
+                Dim dataObj As New List(Of jsonPackageData)
+                Dim designObj As New List(Of jsonPackageData)
+                '
+                Dim recordCount As Integer = 0
+                '
+                If cs.Open("Layouts", "name='People List'", , , "layout") Then
+                    localPackageDesign.dataSet = cs.GetText("layout")
+                    '
+                    designObj.Add(localPackageDesign)
+                End If
+                cs.Close()
+                '
+                If cs.Open("People", , "lastName", , "id,firstName,lastName") Then
+                    recordCount = cs.GetRowCount()
+                    '
+                    Do While cs.OK()
+                        people.id = cs.GetInteger("id")
+                        people.firstName = cs.GetText("firstName")
+                        people.lastName = cs.GetText("lastName")
+                        '
+                        peopleList.Add(people)
+                        '
+                        cs.GoNext()
+                        '
+                        If cs.OK() Then
+                            people = New people
+                        End If
+                    Loop
+                    '
+                    localPackageData.dataFor = "People"
+                    localPackageData.recordCount = recordCount
+                    localPackageData.dataSet = peopleList
+                    '
+                    dataObj.Add(localPackageData)
+                Else
+                    localPackageError.number = 999
+                    localPackageError.description = "no people found"
+                    '
+                    errList.Add(localPackageError)
+                End If
+                cs.Close()
+                '
+                s = getSerializedJSONWrapper(CP, success, errList, dataObj, designObj)
+            Catch ex As Exception
+                Try
+                    CP.Site.ErrorReport(ex, "error in Contensive.Addons.performanceCloud.effectRemotes.mvcProject1HandlerClass.Execute")
+                Catch errObj As Exception
+                End Try
+            End Try
+            '
+            Return s
+        End Function
+        '
+    End Class
+    '
+End Namespace
+
